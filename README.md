8e41461c1ef831764fc2b07296eb80d03d5c476c
eSuardOdin <esuardodin@gmail.com>
2024-02-21 15:53:42 +0100

    Mouais

---
+++ README.md
@@ -0,0 +1,13 @@
+# Raspvision
+
+Little project in order to gain some knowledge about rust and OS / Lowlevel
+Cheating a little bit as i'm using the crate sysinfo
+
+## Sysinfo doc :
+https://docs.rs/sysinfo/latest/sysinfo/
+
+------
+
+Maybe for later if I need to create the wheel once again :
+- Get info about memory -> /proc/meminfo
+- 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use sysinfo::{
+    Components, Disks, Networks, System
+};
+use std::fmt;
+
+
+fn bytes_to_gb(bytes: u64) -> f64
+{
+    bytes as f64 / (1024i32.pow(3) as f64)
+}
+
+
+// Structure perso
+struct Ram
+{
+    used: u64,
+    free: u64,
+    total: u64,
+}
+
+impl fmt::Display for Ram
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        writeln!(f, "Mémoire utilisée : {:.2}/{:.2} Gb", bytes_to_gb(self.used), bytes_to_gb(self.total))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all(); // Permet de tout init
+    sys.refresh_all();
+
+    let mut ram = Ram {
+        used: sys.used_memory(),
+        free: sys.free_memory(),
+        total: sys.total_memory()
+    };
+    println!("{}", ram);
 }
